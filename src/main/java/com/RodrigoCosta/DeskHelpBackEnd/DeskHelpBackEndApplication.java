02ace4dafe7f214c5edfe67bf528a79ab4d55ce9
Rodrigocostta <rhodrigo_costanet@hotmail.com>
2023-10-05 14:25:41 -0300

    inicializaçãoDesk Help

---
+++ src/main/java/com/RodrigoCosta/DeskHelpBackEnd/DeskHelpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.RodrigoCosta.DeskHelpBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskHelpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskHelpBackEndApplication.class, args);
+	}
+
+}
